585ca01d2a3f75d88fd4526f1e760ac5047cfd24
Mohammed-Althaf <mohamedalthaf694@gmail.com>
2024-12-24 10:53:21 +0400

    Intillized the repo

--- main.py
+++ main.py
@@ -3,27 +3,30 @@ from Windows import Windows
 from CyberSceurity import CyberSecurity
 import subprocess as sub 
 
-questions = [
-  inquirer.List
-  (    'Main',
-        message="Choice a branch",
-        choices=['Windows', 
-                  'Linux',
-                  'CyberSecurity' 
-                  ],
-        carousel=True,
-  ),
-]
-answers = inquirer.prompt(questions)
+def main():
+  questions = [
+    inquirer.List
+    (    'Main',
+          message="Choice a branch",
+          choices=['Windows', 
+                    'Linux',
+                    'CyberSecurity' 
+                    ],
+          carousel=True,
+    ),
+  ]
+  answers = inquirer.prompt(questions)
 
-if answers['Main'] == 'Windows':
-  sub.run('cls',shell=True)
-  Windows()
-  
-if answers['Main'] == 'Linux':
-  sub.run('cls',shell=True)
-  Windows()
+  if answers['Main'] == 'Windows':
+    sub.run('cls',shell=True)
+    Windows()
+    
+  if answers['Main'] == 'Linux':
+    sub.run('cls',shell=True)
+    Windows()
 
-if answers['Main'] == 'CyberSecurity':
-  sub.run('cls',shell=True)
-  CyberSecurity()
+  if answers['Main'] == 'CyberSecurity':
+    sub.run('cls',shell=True)
+    CyberSecurity()
+
+main()
